a9ea0b808c5c797488fa94340b45a3a46e1f3230
gummadiakshara-22 <gummadiakshara921@gmail.com>
2025-04-23 09:24:43 +0530

    Update Seat.cpp

--- Seat.cpp
+++ Seat.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
+#include"passenger.cpp"
 class Seat {
-    string passenger_name;
-    int passenger_age;
+    Passenger passenger;
     int seat_number;
     bool booked;
     string reservation_number;
 
 public:
-    Seat() : passenger_name("NULL"), passenger_age(0), seat_number(0), booked(false), reservation_number("") {}
+    Seat() : seat_number(0), booked(false), reservation_number("") {}
 
     bool isBooked() const { return booked; }
     void setSeatNumber(int num) { seat_number = num; }
@@ -16,32 +16,44 @@ public:
     void bookSeat() {
         cin.ignore();
         cout << "Enter Passenger's Name: ";
-        getline(cin, passenger_name);
+        string p;
+        getline(cin, p);
         cout << "Enter Passenger's Age: ";
-        cin >> passenger_age;
+        int a;
+        cin >> a;
+        cout<<"Enter Passenger's Unique ID(up to 4char): ";
+        cin.ignore();
+        string id;
+        getline(cin, id);
+        passenger.setPassenger(p, a, id);
         booked = true;
-        reservation_number = passenger_name.substr(0, 3) + to_string(passenger_age);
-        cout << "Reservation Number: " << reservation_number << endl;
+        setReservationNumber(passenger);
+        cout << "Booking Confirmed. Your PNR is "<< reservation_number << endl;}
+    void setReservationNumber(Passenger p){
+        reservation_number = p.get_passengername().substr(0,3)+p.get_passengerid() ;
     }
-
     void unBookSeat() {
-        passenger_name = "NULL";
-        passenger_age = 0;
+        passenger.setPassenger("NULL", 0 , "NULL");
         booked = false;
         reservation_number = "";
     }
 
     string getReservationNumber() const { return reservation_number; }
     int getSeatNumber() const { return seat_number; }
-    string getPassengerName() const { return passenger_name ;}
-    int getPassengerAge() const { return passenger_age; }
-    void displayDetails(bool isEconomy) const {
-        cout << "Passenger Name: " << passenger_name << endl;
-        cout << "Passenger Age: " << passenger_age << endl;
+    int getindex(string pnr){
+        
+    }
+    void displayDetails(bool isEconomy)  {
+        cout << "Passenger Name: " << passenger.get_passengername() << endl;
+        cout << "Passenger Age: " << passenger.get_passengerage() << endl;
         cout << "Seat Number: " << seat_number << endl;
         cout << "Seat Type: " << (isEconomy ? "Economy" : "Business") << endl;
         cout << "Booked Status: " << (booked ? "Booked" : "Vacant") << endl;
         cout << "Reservation Number: " << reservation_number << endl;
     }
+    
+    string getPassengerName()  { return passenger.get_passengername(); }
+    int getPassengerAge()  { return passenger.get_passengerage(); }
+    string getPassengerID()  { return passenger.get_passengerid(); }
 
 };
